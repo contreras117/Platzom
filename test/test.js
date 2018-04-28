e1d36ad6d91bac88f321ff68a3dc9b894e5ca90b
Daniel Contreras <daniel_contreras117@hotmail.com>
2018-04-28 00:01:06 -0600

    Terminados los test

--- test/test.js
+++ test/test.js
@@ -1,29 +1,26 @@
-/* import platzom from "platzom"
-import chai from "chai" */
-
-const expect = require('chai').expect
+const expect = require('chai').expect /* Importa la funcion expect del modulo 'chai' y la guarda en lña const expect */
 const platzom = require('..').default
 
-describe('#platzom', function () {
-  it('Si la palabra termina en "ar", se le quitan esos dos caracteres.', function() {
+describe('#platzom', () => {
+  it('Si la palabra termina en "ar", se le quitan esos dos caracteres.', () => {
     const translation = platzom("Programar")
     expect(translation).to.equal("Program")
   })
 
-  it('Si la palabra inicia con z, se le añade "pe" al final.', function() {
+  it('Si la palabra inicia con z, se le añade "pe" al final.', ()=> {
     const translation = platzom("Zorro")
     const translation2 = platzom("Zarpar")
     expect(translation).to.equal("Zorrope")
     expect(translation2).to.equal("Zarppe")
   })
 
-  it('Si la palabra traducida tiene mas 10 o mas letras, se parte a la mitad y se une con un guion medio.', function() {
+  it('Si la palabra traducida tiene mas 10 o mas letras, se parte a la mitad y se une con un guion medio.', ()=> {
     const translation = platzom("abecedario")
     expect(translation).to.equal("abece-dario")
   })
 
-  it('Si la palabra original es un palindromo, ninguna regla anterior cuenta y se devuelve la misma palabra intercalando mayusculas y minusculas.', function() {
-    const translation = platzom("Anita lava la tina")
-    expect(translation).to.equal("AnItA lAvA")
+  it('Si la palabra original es un palindromo, ninguna regla anterior cuenta y se devuelve la misma palabra intercalando mayusculas y minusculas.', ()=> {
+    const translation = platzom("sometemos")
+    expect(translation).to.equal("SoMeTeMoS")
   })
 })
